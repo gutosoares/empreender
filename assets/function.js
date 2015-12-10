ceb019e4d1ec910b78b0f080c0fb44a9af0baa19
Augusto Soares <gutosoares@outlook.com>
2015-12-10 00:46:22 -0200

    Criação do arquivo em js

---
+++ assets/function.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('a.mdl-navigation__link').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
